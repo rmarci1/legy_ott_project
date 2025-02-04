66612c6db1e0fe5b38f3217feb90328f2cd2dfb6
Gecse Vanessza <vanessza.gecse@gmail.com>
2025-02-04 09:24:59 +0100

    fixed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,15 +3,30 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useBodyParser('json');
-  
-  
+
+  app.use(
+    session({
+      secret: 'your_secret_key',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: false,
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24
+      },
+    })
+  );
+
   const allowedOrigins = [
-    'http://localhost:5173',  //local webpage
-    'http://192.168.11.142:8081', //     
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://192.168.11.82:3000',
+    'http://192.168.11.142:8081',
   ];
 
 
@@ -29,19 +44,13 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
   });
 
-  app.use((req, res, next) => {
-    console.log("Session middleware:", req.session);
-    next();
-  });
-
-
   app.useGlobalPipes(new ValidationPipe());
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
-  
-  
+
   await app.listen(3000, '0.0.0.0');
 }
+
 bootstrap();
