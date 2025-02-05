6fd553a14387b47b89799df3c44717941e104149
Gecse Vanessza <vanessza.gecse@gmail.com>
2025-02-05 15:50:48 +0100

    one test and get Jobs by categories

--- backend/src/cloudinary/cloudinary.service.ts
+++ backend/src/cloudinary/cloudinary.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
 import { createReadStream, ReadStream } from 'fs';
 import path = require('path');
+import { Readable, Stream } from 'stream';
 @Injectable()
 
 export class CloudinaryService {
   async uploadImage(
-    file?: ReadStream
+    file?: Readable
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     if(file){
         return new Promise((resolve, reject) => {
           const upload = v2.uploader.upload_stream((error, result) => {
-            if (error) return reject(error);
+            if (error){
+              console.error('Error: ' + error);
+              return reject(error);
+            } 
             resolve(result);
           });
         
--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ProfilesService } from 'src/profiles/profiles.service';
+import { ProfilesModule } from 'src/profiles/profiles.module';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Module({
+  imports: [ProfilesModule],
   controllers: [JobsController],
-  providers: [JobsService, PrismaService],
+  providers: [JobsService, PrismaService, ProfilesService, CloudinaryService],
 })
 export class JobsModule {}
