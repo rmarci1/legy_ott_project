80043e6094f826c85c90adea2d1111d96471c2cc
Gecse Vanessza <vanessza.gecse@gmail.com>
2025-02-18 09:12:47 +0100

    Reviews error messages corrected

--- backend/src/reviews/reviews.service.ts
+++ backend/src/reviews/reviews.service.ts
@@ -27,7 +27,7 @@ export class ReviewsService {
       });
     }
     catch{
-      throw new Error("Nem létezik ilyen Profil")
+      throw new Error("Nincsenek értékelések ehhez a profilhoz!")
     }
   }
 
@@ -41,7 +41,7 @@ export class ReviewsService {
       });
     }
     catch {
-      throw new Error("Nem létezik ilyen profil");
+      throw new Error("Nem létezik ilyen értékelés");
     }
   }
 
