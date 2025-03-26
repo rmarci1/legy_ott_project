b87e2a0a710af7b92ed4c704ab1bee5b30276d5a
Gecse Vanessza <vanessza.gecse@gmail.com>
2025-03-26 19:19:53 +0100

    Fix admin page availability

--- weboldal/src/pages/admin/AdminRoute.tsx
+++ weboldal/src/pages/admin/AdminRoute.tsx
@@ -1,11 +1,30 @@
 import { Navigate, Outlet } from "react-router";
 import { useAuth } from "../../context/AuthContext";
+import {useCallback, useEffect, useState} from "react";
 
 export default function AdminRoute(){
-    const {user} = useAuth();
+    const {user, checkUser} = useAuth();
+    const [loading, setLoading] = useState(true);
+
+    const memoizedCheckUser = useCallback(checkUser, []);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            await checkUser();
+            setLoading(false);
+        };
+        fetchUser();
+    }, [memoizedCheckUser]);
+
+    if (loading) {
+        return <div className="flex justify-center w-dvw md:items-center align-center">
+            <div className="animate-spin  rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
+        </div>
+    }
+
     if (!user || !user.isAdmin) {
         return <Navigate to="/" replace/>;
     }
-    
-    return <Outlet />;
+
+    return <Outlet/>;
 }
